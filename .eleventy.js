62ed8e0bb4b6600734757cfe664668003c5199af
Wayne Graham <wgraham@clir.org>
2025-07-18 09:40:53 -0400

    Add minimal files

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,24 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   // Passthrough copy for assets
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
+  eleventyConfig.addFilter("date", (value, format = "yyyy") => {
+    const date = value === "now" ? DateTime.now() : DateTime.fromISO(value);
+    return date.isValid ? date.toFormat(format) : "Invalid date";
+  });
+
   return {
     dir: {
-      input: "src",       // source folder
+      input: "src", // source folder
       includes: "_includes", // default for nunjucks/partials
-      data: "_data",         // data folder (optional)
-      output: "_site"     // build output (default)
+      layouts: "_layouts", // default for nunjucks/layouts
+      data: "_data", // data folder (optional)
+      output: "_site", // build output (default)
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    templateFormats: ["md", "njk", "html"]
+    templateFormats: ["md", "njk", "html"],
   };
 };
