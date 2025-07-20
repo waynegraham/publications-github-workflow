04fd8ff02a91ee335ca8370817745c56c56eb3ab
Wayne Graham <wgraham@clir.org>
2025-07-20 09:47:23 -0400

    Updates to site

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,31 @@
 const { DateTime } = require("luxon");
 
+const { eleventyImageTransformPlugin } = require('@11ty/eleventy-img');
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const eleventyPluginHubspot = require('eleventy-plugin-hubspot');
+const { IdAttributePlugin, HtmlBasePlugin } = require('@11ty/eleventy');
+
+
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.setTemplateFormats(['njk', 'js', 'md', 'html']);
+
+  eleventyConfig.addBundle('css');
+  eleventyConfig.addBundle('js');
+
+   // Preprocessors
+  eleventyConfig.addPreprocessor('drafts', '*', (data, content) => {
+    if (data.draft && process.env.ELEVENTY_RUN_MODE === 'build') {
+      return false;
+    }
+  });
+
   // Passthrough copy for assets
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ 'src/static': './static/' });
+  eleventyConfig.addPassthroughCopy({ 'src/assets/': './assets/' });
+
+  // Watch targets
+  eleventyConfig.addWatchTarget('./src/styles/');
 
   eleventyConfig.addFilter("slug", function (value) {
     if (!value || typeof value !== "string") return "";
@@ -15,6 +38,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("date", (value, format = "yyyy") => {
     const date = value === "now" ? DateTime.now() : DateTime.fromISO(value);
     return date.isValid ? date.toFormat(format) : "Invalid date";
+  });
+
+  eleventyConfig.addFilter('readableDate', (dateObj, format, zone) => {
+    // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+    return DateTime.fromJSDate(dateObj, { zone: zone || 'utc' }).toFormat(format || 'dd LLL yyyy');
+  });
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
   });
 
   // Collections
@@ -73,6 +106,53 @@ eleventyConfig.addCollection("recentVideos", function (collectionApi) {
     .slice(0, 5);
 });
 
+ // Plugins
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ['avif', 'webp', 'jpeg'],
+    // output image widths
+    widths: ['auto'],
+
+    // optional, attributes assigned on <img> nodes override these values
+    htmlOptions: {
+      imgAttributes: {
+        loading: 'lazy',
+        decoding: 'async',
+      },
+      pictureAttributes: {},
+    },
+  });
+
+  //https://www.11ty.dev/docs/plugins/navigation/
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
+  eleventyConfig.addPlugin(eleventyPluginHubspot, {
+    portalId: 20251227,
+    loadingMode: 'lazy',
+  });
+
+  // https://www.11ty.dev/docs/plugins/id-attribute/
+  eleventyConfig.addPlugin(IdAttributePlugin, {
+    selector: 'h1,h2,h3,h4,h5,h6', // default
+
+    // swaps html entities (like &amp;) to their counterparts before slugify-ing
+    decodeEntities: true,
+
+    // check for duplicate `id` attributes in application code?
+    checkDuplicates: 'error', // `false` to disable
+
+    // by default we use Eleventy’s built-in `slugify` filter:
+    slugify: eleventyConfig.getFilter('slugify'),
+
+    filter: function ({ page }) {
+      if (page.inputPath.endsWith('test-skipped.html')) {
+        return false; // skip
+      }
+
+      return true;
+    },
+  });
+
   // Return eleventy
   return {
     dir: {
@@ -84,7 +164,6 @@ eleventyConfig.addCollection("recentVideos", function (collectionApi) {
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    templateFormats: ["md", "njk", "html"],
     pathPrefix: "/publications-github-workflow",
   };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
+  darkMode: 'class',
 }
